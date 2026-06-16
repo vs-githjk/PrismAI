a7524b8aa754d40a9c401201517887c15d40bdd9
Vidyut Sriram <vidyut0712@gmail.com>
2026-06-16 11:22:44 +0530

    Restore topbar back arrow in meeting view

--- frontend/src/components/DashboardPage.jsx
+++ frontend/src/components/DashboardPage.jsx
@@ -955,6 +955,7 @@ export default function DashboardPage(props) {
         title={pageTitle}
         searchValue={props.historySearch}
         onSearchChange={handleHistorySearchChange}
+        onBack={activeView === 'meeting' ? () => persistView('home') : null}
         actions={
           activeView === 'meeting' && props.result && !props.loading ? (
             <MeetingActionsBar
